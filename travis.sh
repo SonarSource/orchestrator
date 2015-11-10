e1af4e1161156e7834eac27a3dcab5137c0a88fd
Julien HENRY <julien.henry@sonarsource.com>
2015-11-10 11:35:27 +0100

    Enable PR analysis

--- travis.sh
+++ travis.sh
@@ -11,4 +11,29 @@ function installTravisTools {
 # We need some private jars like oracle
 installTravisTools
 
-mvn verify -B -e -V -Dmaven.test.redirectTestOutputToFile=false
+if [ -n "${PR_ANALYSIS:-}" ] && [ "${PR_ANALYSIS}" == true ]
+then
+  if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+  then
+    # For security reasons environment variables are not available on the pull requests
+    # coming from outside repositories
+    # http://docs.travis-ci.com/user/pull-requests/#Security-Restrictions-when-testing-Pull-Requests
+    if [ -n "$SONAR_GITHUB_OAUTH" ]; then
+      # PR analysis
+      mvn verify sonar:sonar -B -e -V \
+        -Dsonar.analysis.mode=issues \
+        -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+        -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+        -Dsonar.github.login=$SONAR_GITHUB_LOGIN \
+        -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH \
+        -Dsonar.host.url=$SONAR_HOST_URL \
+        -Dsonar.login=$SONAR_LOGIN \
+        -Dsonar.password=$SONAR_PASSWD  
+    fi
+  fi
+else
+  # Regular CI
+  mvn verify -B -e -V -Dmaven.test.redirectTestOutputToFile=false
+fi
+
+
