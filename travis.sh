0358ec467986e53fc2a387bd83b96be95c086f12
Simon Brandhof <simon.brandhof@sonarsource.com>
2015-11-24 11:57:08 +0100

    Handle analysis of pull requests and master commits

--- travis.sh
+++ travis.sh
@@ -11,29 +11,40 @@ function installTravisTools {
 # We need some private jars like oracle
 installTravisTools
 
-if [ -n "${PR_ANALYSIS:-}" ] && [ "${PR_ANALYSIS}" == true ]
+if [ "${TRAVIS_BRANCH}" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]
 then
-  if [ "$TRAVIS_PULL_REQUEST" != "false" ]
-  then
-    # For security reasons environment variables are not available on the pull requests
-    # coming from outside repositories
-    # http://docs.travis-ci.com/user/pull-requests/#Security-Restrictions-when-testing-Pull-Requests
-    if [ -n "$SONAR_GITHUB_OAUTH" ]; then
-      # PR analysis
-      mvn verify sonar:sonar -B -e -V \
-        -Dsonar.analysis.mode=issues \
-        -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
-        -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-        -Dsonar.github.login=$SONAR_GITHUB_LOGIN \
-        -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH \
-        -Dsonar.host.url=$SONAR_HOST_URL \
-        -Dsonar.login=$SONAR_LOGIN \
-        -Dsonar.password=$SONAR_PASSWD  
-    fi
-  fi
+  # this commit is master must be built and analyzed (with upload of report)
+  mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify -Pcoverage-per-test -B -e -V
+
+  # Switch to java 8 as the Dory HTTPS certificate is not supported by Java 7
+  export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+  export PATH=$JAVA_HOME/bin:$PATH
+
+  mvn sonar:sonar -B -e -V \
+     -Dsonar.host.url=$SONAR_HOST_URL \
+     -Dsonar.login=$SONAR_LOGIN \
+     -Dsonar.password=$SONAR_PASSWORD
+
+
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ]
+  # this pull request must be built and analyzed (without upload of report)
+  mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify -Pcoverage-per-test -B -e -V
+
+  # Switch to java 8 as the Dory HTTPS certificate is not supported by Java 7
+  export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+  export PATH=$JAVA_HOME/bin:$PATH
+
+  mvn sonar:sonar -B -e -V \
+      -Dsonar.analysis.mode=issues \
+      -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+      -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+      -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH \
+      -Dsonar.host.url=$SONAR_HOST_URL \
+      -Dsonar.login=$SONAR_LOGIN \
+      -Dsonar.password=$SONAR_PASSWORD
+
+
 else
-  # Regular CI
+  # Build branch, without any analysis
   mvn verify -B -e -V -Dmaven.test.redirectTestOutputToFile=false
 fi
-
-
