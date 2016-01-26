729eb21fbb2766f5078011cda56dd15a00958472
Simon Brandhof <simon.brandhof@sonarsource.com>
2016-01-26 15:34:39 +0100

    Use repox as Maven remote repo
    That allows to get Artifactory maven plugin

--- travis.sh
+++ travis.sh
@@ -12,29 +12,36 @@ function installTravisTools {
 installTravisTools
 
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  echo 'Build and analyze commit in master'
+  echo 'Build, deploy and analyze commit in master'
   mvn org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar \
-    -Pcoverage-per-test,deploy-artifactory \
+    -Pcoverage-per-test,repox,deploy-artifactory \
     -Dmaven.test.redirectTestOutputToFile=false \
     -Dsonar.host.url=$SONAR_HOST_URL \
     -Dsonar.login=$SONAR_TOKEN \
     -Dartifactory.deploy.username=$REPOX_DEPLOY_USERNAME \
     -Dartifactory.deploy.password=$REPOX_DEPLOY_PASSWORD \
-    -B -e -V
+    -B -e -V \
+    -s settings-repox.xml
 
 elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN-}" ]; then
   echo 'Build and analyze pull request'
   mvn org.jacoco:jacoco-maven-plugin:prepare-agent verify \
-    -Pcoverage-per-test -Dmaven.test.redirectTestOutputToFile=false -B -e -V \
+    -Pcoverage-per-test,repox \
+    -Dmaven.test.redirectTestOutputToFile=false \
     -Dsonar.analysis.mode=issues \
     -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
     -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
     -Dsonar.github.oauth=$GITHUB_TOKEN \
     -Dsonar.host.url=$SONAR_HOST_URL \
-    -Dsonar.login=$SONAR_TOKEN
+    -Dsonar.login=$SONAR_TOKEN \
+    -B -e -V \
+    -s settings-repox.xml
     
 else
-  echo 'Build, no analysis'
+  echo 'Build, no deploy, no analysis'
   # Build branch, without any analysis
-  mvn verify -Dmaven.test.redirectTestOutputToFile=false -B -e -V
+  mvn verify \
+    -Dmaven.test.redirectTestOutputToFile=false \
+    -B -e -V \
+    -s settings-repox.xml
 fi
