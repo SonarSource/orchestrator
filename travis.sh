302348d745040ca431a68fa02b463673b14295c7
Simon Brandhof <simon.brandhof@sonarsource.com>
2016-08-03 15:19:47 +0200

    Upgrade to travis-utils 31

--- travis.sh
+++ travis.sh
@@ -4,9 +4,10 @@ set -euo pipefail
 
 function configureTravis {
   mkdir ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v28 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v31 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 configureTravis
+. installJDK8
 
 regular_mvn_build_deploy_analyze
