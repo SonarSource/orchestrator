a2b2cdd2ac126db2783fb6dbfe27a608ed0bb558
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2016-11-17 17:36:50 +0100

    Revert "Proxy authentication support"
    This reverts commit 5ee966b9ca8ef142e5098b32b8e68fa63d9008f3.
    
    support for HTTP proxy was lacking UT which, when added, highlighted that the provided implementation does not work
    (technical detail: the ProxyAuthenticator is set on the Ok HTTP connection but the Proxy is missing, resulting in "java.net.ProtocolException: Received HTTP_PROXY_AUTH (407) code while not using proxy" error)

--- sonar-orchestrator/src/main/java/com/sonar/orchestrator/locator/URLLocator.java
+++ sonar-orchestrator/src/main/java/com/sonar/orchestrator/locator/URLLocator.java
@@ -22,17 +22,13 @@ package com.sonar.orchestrator.locator;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 import javax.annotation.CheckForNull;
 import javax.annotation.Nullable;
-import okhttp3.Authenticator;
-import okhttp3.Credentials;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import okhttp3.Route;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
@@ -105,24 +101,9 @@ class URLLocator implements Locator<URLLocation> {
   private static Response sendHttpRequest(URLLocation location) throws IOException {
     LOG.info("Downloading: " + location.getURL());
 
-    OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder().readTimeout(60, TimeUnit.SECONDS);
-
-    // OkHttp detect 'http.proxyHost' java property, but credentials should be filled
-    final String proxyUser = System.getProperty("http.proxyUser");
-    if (StringUtils.isNotBlank(System.getProperty("http.proxyHost")) && StringUtils.isNotBlank(proxyUser)) {
-      httpClientBuilder.proxyAuthenticator(new Authenticator() {
-        @Override
-        public Request authenticate(Route route, Response response) throws IOException {
-          if (HttpURLConnection.HTTP_PROXY_AUTH == response.code()) {
-            String credential = Credentials.basic(proxyUser, System.getProperty("http.proxyPassword"));
-            return response.request().newBuilder().header("Proxy-Authorization", credential).build();
-          }
-          return null;
-        }
-      });
-    }
-
-    OkHttpClient httpClient = httpClientBuilder.build();
+    OkHttpClient httpClient = new OkHttpClient.Builder()
+      .readTimeout(60, TimeUnit.SECONDS)
+      .build();
     Request httpRequest = new Request.Builder()
       .url(location.getURL())
       .header("User-Agent", USER_AGENT)
