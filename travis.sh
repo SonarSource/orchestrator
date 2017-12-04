b7356d11c7f7007bfd5fccab5368920ca0026c3d
Simon Brandhof <simon.brandhof@sonarsource.com>
2017-12-04 10:30:50 +0100

    Disable build of branch on Travis if open PR

--- travis.sh
+++ travis.sh
@@ -4,11 +4,13 @@ set -euo pipefail
 
 function configureTravis {
   mkdir -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v38 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v41 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 configureTravis
 . installJDK8
+
+cancel_branch_build_with_pr || if [[ $? -eq 1 ]]; then exit 0; fi
 
 export DEPLOY_PULL_REQUEST=true
 
