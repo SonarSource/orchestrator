145adc4edba0a9b6a70b9d88ff7fe9a11fefe4b6
Simon Brandhof <simon.brandhof@sonarsource.com>
2018-02-21 18:28:09 +0100

    ORCH-410 refactor file system

--- sonar-orchestrator/src/main/java/com/sonar/orchestrator/config/FileSystem.java
+++ sonar-orchestrator/src/main/java/com/sonar/orchestrator/config/FileSystem.java
@@ -23,120 +23,37 @@ import com.sonar.orchestrator.locator.Location;
 import com.sonar.orchestrator.locator.Locators;
 import java.io.File;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import javax.annotation.CheckForNull;
-import org.apache.commons.lang.SystemUtils;
-import org.slf4j.LoggerFactory;
+import javax.annotation.Nullable;
 
-import static com.sonar.orchestrator.util.OrchestratorUtils.isEmpty;
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+import static org.apache.commons.io.FileUtils.getUserDirectory;
 
 public class FileSystem {
-  private final Configuration config;
   private final Locators locators;
-  private File mavenLocalRepository;
+  private File orchestratorHome;
+  @Nullable
   private File mavenHome;
-  private String mavenBinary;
-  private File antHome;
-  private File javaHome;
+  private File mavenLocalRepository;
+  private File sonarQubeZipsDir;
   private File workspace;
-  private File sonarInstallsDir;
+  @Nullable
+  private File javaHome;
+  @Nullable
+  private File antHome;
 
   public FileSystem(Configuration config) {
-    this.config = config;
     this.locators = new Locators(config);
-    initWorkspace();
-    initMavenHome();
-    initMavenBinary();
-    initAntHome();
-    initUserHome();
-    initJavaHome();
-    initMavenLocalRepository();
-  }
-
-  private void initWorkspace() {
-    String value = config.getString("orchestrator.workspaceDir", "target");
-    workspace = new File(value);
-  }
-
-  private void initMavenLocalRepository() {
-    String value = config.getStringByKeys(s -> !isEmpty(s), "maven.localRepository", "MAVEN_LOCAL_REPOSITORY");
-    if (value != null) {
-      mavenLocalRepository = new File(value);
-      if (!mavenLocalRepository.isDirectory() || !mavenLocalRepository.exists()) {
-        throw new IllegalArgumentException("Maven local repository is not valid: " + value);
-      }
-    }
-  }
-
-  private void initJavaHome() {
-    String value = config.getStringByKeys(s -> !isEmpty(s), "java.home", "JAVA_HOME");
-    if (value != null) {
-      javaHome = new File(value);
-      if (!javaHome.isDirectory() || !javaHome.exists()) {
-        throw new IllegalArgumentException("Java home is not valid: " + value);
-      }
-    }
-    if (javaHome == null) {
-      LoggerFactory.getLogger(FileSystem.class).warn("Java home is not set. Please set the property java.home or the env variable JAVA_HOME");
-    }
-  }
-
-  private void initUserHome() {
-    String value = config.getString("orchestrator.sonarInstallsDir");
-    if (!isEmpty(value)) {
-      sonarInstallsDir = new File(value);
-    } else {
-      String sonarUserHome = System.getenv("SONAR_USER_HOME");
-      if (!isEmpty(sonarUserHome)) {
-        sonarInstallsDir = new File(sonarUserHome, "installs");
-      } else {
-        File userHome = new File(System.getProperty("user.home"));
-        sonarInstallsDir = new File(userHome, ".sonar/installs");
-      }
-    }
-  }
-
-  private void initMavenHome() {
-    String propertyValue = config.getStringByKeys(s -> !isEmpty(s), "maven.home", "MAVEN_HOME", "M2_HOME");
-    if (propertyValue != null) {
-      mavenHome = new File(propertyValue);
-      if (!mavenHome.isDirectory() || !mavenHome.exists()) {
-        throw new IllegalArgumentException("Maven home is not valid: " + propertyValue);
-      }
-    }
-  }
-
-  private void initMavenBinary() {
-    String binary = config.getStringByKeys(s -> !isEmpty(s), "maven.binary", "MAVEN_BINARY");
-    if (binary != null) {
-      mavenBinary = binary;
-      File completePath = new File(mavenHome, "bin/" + mavenBinary);
-      List<String> binaryExtensions = new ArrayList<>();
-      binaryExtensions.add("");
-      if (SystemUtils.IS_OS_WINDOWS) {
-        binaryExtensions.add(".cmd");
-        binaryExtensions.add(".bat");
-      }
-      for (String ext : binaryExtensions) {
-        File completePathWithExt = new File(completePath + ext);
-        if (completePathWithExt.isFile() && completePathWithExt.exists()) {
-          return;
-        }
-      }
-      throw new IllegalArgumentException(
-        "Maven binary is not valid: " + completePath.getAbsolutePath() + " (With one of these extensions: " + binaryExtensions + ")");
-    }
-  }
-
-  private void initAntHome() {
-    String value = config.getStringByKeys("ant.home", "ANT_HOME");
-    if (!isEmpty(value)) {
-      antHome = new File(value);
-      if (!antHome.isDirectory() || !antHome.exists()) {
-        throw new IllegalArgumentException("Ant home is not valid: " + value);
-      }
-    }
+    this.orchestratorHome = initDir(config, singletonList("SONAR_USER_HOME"), new File(getUserDirectory(), ".sonar"));
+    this.mavenHome = initDir(config, asList("maven.home", "MAVEN_HOME"), null);
+    this.mavenLocalRepository = initDir(config, asList("maven.localRepository", "MAVEN_LOCAL_REPOSITORY"), new File(getUserDirectory(), ".m2/repository"));
+    this.sonarQubeZipsDir = initDir(config, singletonList("orchestrator.sonarInstallsDir"), new File(orchestratorHome, "installs"));
+    this.workspace = initDir(config, singletonList("orchestrator.workspaceDir"), new File("target"));
+    this.javaHome = initDir(config, asList("java.home", "JAVA_HOME"), null);
+    this.antHome = initDir(config, asList("ant.home", "ANT_HOME"), null);
   }
 
   public File mavenLocalRepository() {
@@ -149,25 +66,25 @@ public class FileSystem {
   }
 
   @CheckForNull
-  public String mavenBinary() {
-    return mavenBinary;
-  }
-
-  @CheckForNull
   public File antHome() {
     return antHome;
   }
 
+  @CheckForNull
   public File javaHome() {
     return javaHome;
   }
 
-  public File workspace() {
-    return workspace;
+  public File getOrchestratorHome() {
+    return orchestratorHome;
   }
 
-  public File sonarInstallsDir() {
-    return sonarInstallsDir;
+  public File getSonarQubeZipsDir() {
+    return sonarQubeZipsDir;
+  }
+
+  public File workspace() {
+    return workspace;
   }
 
   public File locate(Location location) {
@@ -184,5 +101,15 @@ public class FileSystem {
 
   public File copyToFile(Location location, File toFile) {
     return locators.copyToFile(location, toFile);
+  }
+
+  @CheckForNull
+  private static File initDir(Configuration config, List<String> propertyKeys, @Nullable File defaultFile) {
+    return propertyKeys.stream()
+      .map(config::getString)
+      .filter(Objects::nonNull)
+      .findFirst()
+      .map(File::new)
+      .orElse(defaultFile);
   }
 }
