a5919ddfdb1f2e8bf82f7c5bc5673bd1aa167e14
Julien HENRY <julien.henry@sonarsource.com>
2020-03-27 16:17:07 +0100

    Update the build

--- travis.sh
+++ travis.sh
@@ -2,17 +2,19 @@
 
 set -euo pipefail
 
-function configureTravis {
+function installTravisTools {
   mkdir -p ~/.local
   curl -sSL https://github.com/SonarSource/travis-utils/tarball/v58 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
+  source ~/.local/bin/setup_promote_environment
 }
-configureTravis
+installTravisTools
+source ~/.local/bin/installMaven35
 
 cancel_branch_build_with_pr || if [[ $? -eq 1 ]]; then exit 0; fi
 
 export DEPLOY_PULL_REQUEST=true
-
-regular_mvn_build_deploy_analyze
+source regular_mvn_build_deploy_analyze
+promote
 
 ./check-license-compliance.sh
