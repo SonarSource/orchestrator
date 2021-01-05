13b50b7318d7ea5b646eae6e6d62d8824906b14b
Wouter Admiraal <45544358+wouter-admiraal-sonarsource@users.noreply.github.com>
2021-01-05 13:50:06 +0100

    Use latest travis utils

--- travis.sh
+++ travis.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 function installTravisTools {
   mkdir -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v58 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v60 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
   source ~/.local/bin/setup_promote_environment
 }
