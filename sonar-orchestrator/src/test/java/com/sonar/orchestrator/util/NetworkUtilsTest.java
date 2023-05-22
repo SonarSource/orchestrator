7bed9e1cc4ce596ea2bb2b9d6bd738b44efc1bc0
Julien HENRY <julien.henry@sonarsource.com>
2023-05-22 17:17:02 +0200

    Update mockito

--- sonar-orchestrator/src/test/java/com/sonar/orchestrator/util/NetworkUtilsTest.java
+++ sonar-orchestrator/src/test/java/com/sonar/orchestrator/util/NetworkUtilsTest.java
@@ -29,7 +29,7 @@ import org.junit.rules.ExpectedException;
 import static com.sonar.orchestrator.util.NetworkUtils.getNextAvailablePort;
 import static java.net.InetAddress.getLoopbackAddress;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
