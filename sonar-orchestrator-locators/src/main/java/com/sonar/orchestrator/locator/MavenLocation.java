877ae2e4953a523a014a190eefafcbc29bfe6a7f
Julien HENRY <julien.henry@sonarsource.com>
2025-09-30 15:04:49 +0200

    Extract locators to their own module

---
+++ sonar-orchestrator-locators/src/main/java/com/sonar/orchestrator/locator/MavenLocation.java
@@ -0,0 +1,180 @@
+/*
+ * Orchestrator Locators
+ * Copyright (C) 2011-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package com.sonar.orchestrator.locator;
+
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import static com.sonar.orchestrator.locator.Utils.checkArgument;
+import static org.apache.commons.lang3.StringUtils.isEmpty;
+
+public class MavenLocation implements Location {
+
+  private String groupId;
+  private String artifactId;
+  private String version;
+  private String filename;
+  private String packaging;
+  private String classifier;
+
+  public MavenLocation(Builder builder) {
+    this.groupId = builder.groupId;
+    this.artifactId = builder.artifactId;
+    this.version = builder.version;
+    this.packaging = builder.packaging;
+    this.filename = builder.filename;
+    this.classifier = StringUtils.trimToEmpty(builder.classifier);
+    if (isEmpty(filename)) {
+      if (isEmpty(classifier)) {
+        filename = artifactId + "-" + version + "." + packaging;
+      } else {
+        filename = artifactId + "-" + version + "-" + classifier + "." + packaging;
+      }
+    }
+  }
+
+  public static MavenLocation create(String groupId, String artifactId, String version) {
+    return builder().setGroupId(groupId).setArtifactId(artifactId).setVersion(version).build();
+  }
+
+  public static MavenLocation create(String groupId, String artifactId, String version, String classifier) {
+    return builder().setGroupId(groupId).setArtifactId(artifactId).setVersion(version).setClassifier(classifier).build();
+  }
+
+  /**
+   * @since 2.10.1. Shortcut for {@link #create(String, String, String)}
+   */
+  public static MavenLocation of(String groupId, String artifactId, String version) {
+    return create(groupId, artifactId, version);
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  public String getGroupId() {
+    return groupId;
+  }
+
+  public String getArtifactId() {
+    return artifactId;
+  }
+
+  public String getVersion() {
+    return version;
+  }
+
+  public String getFilename() {
+    return filename;
+  }
+
+  public String getPackaging() {
+    return packaging;
+  }
+
+  public String getClassifier() {
+    return classifier;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    MavenLocation that = (MavenLocation) o;
+    return groupId.equals(that.groupId) && artifactId.equals(that.artifactId) && packaging.equals(that.packaging) && version.equals(that.version);
+  }
+
+  @Override
+  public int hashCode() {
+    int result = groupId.hashCode();
+    result = 31 * result + artifactId.hashCode();
+    result = 31 * result + version.hashCode();
+    result = 31 * result + packaging.hashCode();
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    if (isEmpty(classifier)) {
+      return String.format("[%s:%s:%s:%s]", groupId, artifactId, version, packaging);
+    } else {
+      return String.format("[%s:%s:%s:%s:%s]", groupId, artifactId, version, classifier, packaging);
+    }
+  }
+
+  public static class Builder<G extends Builder<G>> {
+    private String groupId;
+    private String artifactId;
+    private String version;
+    private String filename = null;
+    private String classifier;
+    private String packaging = "jar";
+
+    Builder() {
+    }
+
+    public G setGroupId(String groupId) {
+      this.groupId = groupId;
+      return (G) this;
+    }
+
+    public G setArtifactId(String artifactId) {
+      this.artifactId = artifactId;
+      return (G) this;
+    }
+
+    public G setVersion(String s) {
+      this.version = s;
+      return (G) this;
+    }
+
+    public G setClassifier(String classifier) {
+      this.classifier = classifier;
+      return (G) this;
+    }
+
+    public G setKey(String groupId, String artifactId, String version) {
+      return setGroupId(groupId).setArtifactId(artifactId).setVersion(version);
+    }
+
+    public G withFilename(String s) {
+      this.filename = s;
+      return (G) this;
+    }
+
+    public G withPackaging(String s) {
+      this.packaging = s;
+      return (G) this;
+    }
+
+    public MavenLocation build() {
+      checkArgument(!isEmpty(groupId), "Group Id must be set");
+      checkArgument(!isEmpty(artifactId), "Artifact Id must be set");
+      checkArgument(!isEmpty(ObjectUtils.toString(version)), "Version must be set");
+      checkArgument(!isEmpty(packaging), "Packaging must be set");
+      return new MavenLocation(this);
+    }
+
+  }
+}
---
+++ sonar-orchestrator-locators/src/main/java/com/sonar/orchestrator/locator/MavenLocator.java
@@ -0,0 +1,171 @@
+/*
+ * Orchestrator Locators
+ * Copyright (C) 2011-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package com.sonar.orchestrator.locator;
+
+import com.sonar.orchestrator.config.FileSystem;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.Optional;
+import javax.annotation.CheckForNull;
+import javax.annotation.Nullable;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.Strings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MavenLocator implements Locator<MavenLocation> {
+  private static final Logger LOG = LoggerFactory.getLogger(MavenLocator.class);
+
+  private final FileSystem fileSystem;
+  private final Artifactory artifactory;
+
+  public MavenLocator(FileSystem fileSystem, Artifactory artifactory) {
+    this.fileSystem = fileSystem;
+    this.artifactory = artifactory;
+  }
+
+  private static String pathInMavenLocalRepository(MavenLocation location) {
+    return Strings.CS.replace(location.getGroupId(), ".", "/") + "/" + location.getArtifactId() + "/" + location.getVersion() + "/" + location.getFilename();
+  }
+
+  static String cacheKeyOf(MavenLocation location) {
+    return DigestUtils.md5Hex(location.toString());
+  }
+
+  @Override
+  public File locate(MavenLocation location) {
+    // resolve the version alias if needed (requires to be online)
+    MavenLocation resolvedLocation = resolveLocation(location);
+    return locateResolvedVersion(resolvedLocation);
+  }
+
+  @Nullable
+  File locateResolvedVersion(MavenLocation resolvedLocation) {
+    // check local cache
+    String cacheKey = cacheKeyOf(resolvedLocation);
+    Path cachedDir = fileSystem.getCacheDir().resolve(cacheKey);
+    if (Files.exists(cachedDir)) {
+      Collection<File> files = FileUtils.listFiles(cachedDir.toFile(), null, false);
+      if (files.size() == 1) {
+        File file = files.iterator().next();
+        LOG.info("Found {} at {}", resolvedLocation, file);
+        return file;
+      }
+    }
+
+    // check Maven local repository, if defined.
+    File file = locateInLocalRepository(resolvedLocation);
+    if (file != null) {
+      LOG.info("Found {} in Maven local repository at {}", resolvedLocation, file);
+      return file;
+    }
+
+    // download from Artifactory.
+    // No need to try if SNAPSHOT, only releases are deployed.
+    if (resolvedLocation.getVersion().endsWith("-SNAPSHOT")) {
+      return null;
+    }
+    Path cachedFile = cachedDir.resolve(resolvedLocation.getFilename());
+    boolean found = artifactory.downloadToFile(resolvedLocation, cachedFile.toFile());
+    return found ? cachedFile.toFile() : null;
+  }
+
+  private MavenLocation resolveLocation(MavenLocation location) {
+    Optional<String> version = resolveVersion(location);
+    if (!version.isPresent()) {
+      throw new IllegalStateException("Version can not be resolved: " + location);
+    }
+    if (version.get().equals(location.getVersion())) {
+      return location;
+    }
+    return MavenLocation.builder()
+      .setGroupId(location.getGroupId())
+      .setArtifactId(location.getArtifactId())
+      .setClassifier(location.getClassifier())
+      .withPackaging(location.getPackaging())
+      .setVersion(version.get())
+      .build();
+  }
+
+  public Optional<String> resolveVersion(MavenLocation location) {
+    return artifactory.resolveVersion(location);
+  }
+
+  @CheckForNull
+  private File locateInLocalRepository(MavenLocation location) {
+    if (fileSystem.mavenLocalRepository() != null && Files.exists(fileSystem.mavenLocalRepository())) {
+      Path file = fileSystem.mavenLocalRepository().resolve(pathInMavenLocalRepository(location));
+      if (Files.exists(file)) {
+        return file.toFile();
+      }
+    }
+    return null;
+  }
+
+  @Override
+  public File copyToDirectory(MavenLocation location, File toDir) {
+    File target = locate(location);
+    if (target == null) {
+      return null;
+    }
+
+    try {
+      FileUtils.copyFileToDirectory(target, toDir);
+      return new File(toDir, target.getName());
+    } catch (IOException e) {
+      throw new IllegalStateException("Fail to copy file to dir: " + toDir, e);
+    }
+  }
+
+  @Override
+  public File copyToFile(MavenLocation location, File toFile) {
+    File target = locate(location);
+    if (target == null) {
+      return null;
+    }
+
+    try {
+      FileUtils.copyFile(target, toFile);
+      return toFile;
+    } catch (IOException e) {
+      throw new IllegalStateException("Fail to copy to file: " + toFile, e);
+    }
+  }
+
+  @Override
+  @CheckForNull
+  public InputStream openInputStream(MavenLocation location) {
+    File target = locate(location);
+    if (target == null) {
+      return null;
+    }
+
+    try {
+      return FileUtils.openInputStream(target);
+    } catch (IOException e) {
+      throw new IllegalStateException("Fail to open " + target, e);
+    }
+  }
+}
