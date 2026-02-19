a5d1539cdac3e06e396786956a025a8b7d8be193
Manuel Vianello <manuel.vianello@sonarsource.com>
2026-02-19 16:54:46 +0100

    NO-JIRA h_AZmJX1o2U8CNVV2bgk99: Fix java:S1943

--- sonar-orchestrator-utils/src/main/java/com/sonar/orchestrator/util/command/CommandExecutor.java
+++ sonar-orchestrator-utils/src/main/java/com/sonar/orchestrator/util/command/CommandExecutor.java
@@ -25,6 +25,7 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -176,7 +177,7 @@ public class CommandExecutor {
 
     @Override
     public void run() {
-      try (InputStreamReader isr = new InputStreamReader(is);
+      try (InputStreamReader isr = new InputStreamReader(is, Charset.defaultCharset());
            BufferedReader br = new BufferedReader(isr)) {
         String line;
         while ((line = br.readLine()) != null) {
